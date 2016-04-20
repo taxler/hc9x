30f33b4a47d18075b09b4bf9e84594648ca455b0
Duncan Cross <duncan.cross@gmail.com>
2016-04-20 21:13:33 +0100

    Use span-level styling

--- src/mswindows/WRIExpedition.js
+++ src/mswindows/WRIExpedition.js
@@ -1,4 +1,4 @@
-define(['Promise', '../LegacyExplorer'], function(Promise, LegacyExplorer) {
+define(['Promise', '../LegacyExplorer', '../RangeSpec'], function(Promise, LegacyExplorer, RangeSpec) {
 
 	'use strict';
 
@@ -36,94 +36,143 @@ define(['Promise', '../LegacyExplorer'], function(Promise, LegacyExplorer) {
 				})
 				.then(function(values) {
 					var bytes = values[0], bytes2 = values[1];
-					var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.length);
-					self.textByteLength = dv.getUint32(0, true);
-					self.firstParagraphInfoPageNumber = dv.getUint16(4, true);
-					self.footnoteTablePageNumber = dv.getUint16(6, true);
-					self.sectionPropertyPageNumber = dv.getUint16(8, true);
-					self.sectionTablePageNumber = dv.getUint16(10, true);
-					self.pageTablePageNumber = dv.getUint16(12, true);
-					self.fontFaceNameTablePageNumber = dv.getUint16(14, true);
-					self.pageCount = new DataView(bytes2.buffer, 0, 2).getUint16(0, true);
-					if (self.firstParagraphInfoPageNumber === self.footnoteTablePageNumber) {
-						delete self.firstParagraphInfoPageNumber;
-					}
-					else {
-						self.lastParagraphInfoPageNumber = self.footnoteTablePageNumber - 1;
-					}
-					if (self.footnoteTablePageNumber === self.sectionPropertyPageNumber) {
-						delete self.footnoteTablePageNumber;
-					}
-					if (self.sectionPropertyPageNumber === self.sectionTablePageNumber) {
-						delete self.sectionPropertyPageNumber;
-					}
-					if (self.sectionTablePageNumber === self.pageTablePageNumber) {
-						delete self.sectionTablePageNumber;
-					}
-					if (self.pageTablePageNumber === self.fontFaceNameTablePageNumber) {
-						delete self.pageTablePageNumber;
-					}
-					if (self.fontFaceNameTablePageNumber === self.pageCount) {
-						delete self.fontFaceNameTablePageNumber;
-					}
-					if (self.pageCount === 0) {
+
+					self.endOfData = 128 * new DataView(bytes2.buffer, 0, 2).getUint16(0, true);
+
+					if (self.endOfData === 0) {
 						return Promise.reject('Word files are not supported');
 					}
-					function handleParagraphInfoPage(bytes) {
-						var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
-						var pos = dv.getUint32(0, true);
-						var promises = [];
-						for (var fod_i = 0, fodCount = bytes[127]; fod_i < fodCount; fod_i++) {
-							var afterPos = dv.getUint32(4 + fod_i * 6, true);
-							var fpropOffset = dv.getInt16(4 + fod_i * 6 + 4, true);
-							var fprop;
-							if (fpropOffset !== -1) {
-								fpropOffset += 4;
-								fprop = new Uint8Array(PAP_DEFAULT);
-								fprop.set(bytes.subarray(fpropOffset + 1, fpropOffset + 1 + bytes[fpropOffset]));
+
+					var dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.length);
+
+					self.textOffset = 128;
+					self.textByteLength = dv.getUint32(0, true);
+
+					self.charInfoOffset = 128 * Math.ceil(1 + self.textByteLength / 128);
+					self.paragraphInfoOffset = 128 * dv.getUint16(4, true);
+					self.footnoteTableOffset = 128 * dv.getUint16(6, true);
+					self.sectionPropertyOffset = 128 * dv.getUint16(8, true);
+					self.sectionTableOffset = 128 * dv.getUint16(10, true);
+					self.pageTableOffset = 128 * dv.getUint16(12, true);
+					self.fontFaceNameTableOffset = 128 * dv.getUint16(14, true);
+
+					self.charInfoByteLength = self.paragraphInfoOffset - self.charInfoOffset;
+					self.paragraphInfoByteLength = self.footnoteTableOffset - self.paragraphInfoOffset;
+					self.footnoteTableByteLength = self.sectionPropertyOffset - self.footnoteTableOffset;
+					self.sectionPropertyByteLength = self.sectionTableOffset - self.sectionPropertyByteLength;
+					self.sectionTableByteLength = self.pageTableOffset - self.sectionTableOffset;
+					self.pageTableByteLength = self.fontFaceNameTableOffset - self.pageTableOffset;
+					self.fontFaceNameTableByteLength = self.endOfData - self.fontFaceNameTableOffset;
+
+					function getInfo(bytes, defaultInfo) {
+						var results = new RangeSpec.Set();
+						for (var i_page = 0, max_page = bytes.length / 128; i_page < max_page; i_page++) {
+							var dv = new DataView(bytes.buffer, bytes.byteOffset + i_page * 128, 128);
+							var pos = dv.getUint32(0, true) - 128;
+							for (var fod_i = 0, fodCount = bytes[i_page * 128 + 127]; fod_i < fodCount; fod_i++) {
+								var afterPos = dv.getUint32(4 + fod_i * 6, true) - 128;
+								var fpropOffset = dv.getInt16(4 + fod_i * 6 + 4, true);
+								var fprop;
+								if (fpropOffset === -1) {
+									fprop = defaultInfo;
+								}
+								else {
+									fprop = new Uint8Array(defaultInfo);
+									fpropOffset += i_page * 128 + 4;
+									fprop.set(bytes.subarray(fpropOffset + 1, fpropOffset + 1 + bytes[fpropOffset]));
+								}
+								var range = new RangeSpec(pos, afterPos - pos);
+								range.userdata = fprop;
+								results.put(range);
+								pos = afterPos;
 							}
-							else {
-								fprop = PAP_DEFAULT;
-							}
-							promises.push(Promise.all([Promise.resolve(fprop), self.byteSource.getBytes(pos, afterPos - pos)])
-								.then(function(values) {
-									var fprop = values[0], bytes = values[1];
-									if ((fprop[16] & 16) !== 0) {
-										// TODO: image
-										return document.createElement('DIV');
-									}
-									else {
-										var text = String.fromCharCode.apply(null, bytes);
-										var justify = fprop[1] & 3;
-										var paragraph = document.createElement('DIV');
-										switch(justify) {
-											case 1: paragraph.style.textAlign = 'center'; break;
-											case 2: paragraph.style.textAlign = 'right'; break;
-											case 3: paragraph.style.textAlign = 'justify'; break;
-										}
-										paragraph.appendChild(document.createTextNode(text));
-										paragraph.innerHTML = paragraph.innerHTML.replace(/\n/g, '<br>');
-										return paragraph;
-									}
-								}));
-							pos = afterPos;
 						}
-						return Promise.all(promises);
+						return results;
 					}
-					var promises = [];
-					if ('firstParagraphInfoPageNumber' in self) {
-						for (var page = self.firstParagraphInfoPageNumber; page <= self.lastParagraphInfoPageNumber; page++) {
-							promises.push(self.byteSource.getBytes(128 * page, 128).then(handleParagraphInfoPage));
+
+					return Promise.all([
+						self.byteSource.getBytes(self.textOffset, self.textByteLength),
+						self.byteSource.getBytes(self.charInfoOffset, self.charInfoByteLength)
+							.then(function(charInfoBytes) {
+								return getInfo(charInfoBytes, CHP_DEFAULT);
+							}),
+						self.byteSource.getBytes(self.paragraphInfoOffset, self.paragraphInfoByteLength)
+							.then(function(paragraphInfoBytes) {
+								return getInfo(paragraphInfoBytes, PAP_DEFAULT);
+							})
+					]);
+				})
+				.then(function(values) {
+					var textData = values[0], charInfo = values[1], paragraphInfo = values[2];
+					charInfo.ranges = charInfo.ranges.filter(function(a) { return a.userdata !== CHP_DEFAULT; });
+					charInfo.ranges.forEach(function(a) {
+						var info = a.userdata;
+						a.userdata = {};
+						var styleSet = [];
+						if ((info[1] & 1) !== 0) {
+							styleSet.push('font-weight: bold');
 						}
-					}
-					return Promise.all(promises)
-						.then(function(parts) {
-							for (var i = 0; i < parts.length; i++) {
-								for (var j = 0; j < parts[i].length; j++) {
-									self.article.appendChild(parts[i][j]);
+						if ((info[1] & 2) !== 0) {
+							styleSet.push('font-style: italic');
+						}
+						if (info[2] !== 24) {
+							styleSet.push('font-size: ' + Math.floor((info[2] * 100) / 24) + '%');
+						}
+						var fontCode = (info[1] >> 2) | ((info[4] & 3) << 6);
+						if (info[3] & 1) {
+							styleSet.push('text-decoration: underline');
+						}
+						a.userdata.style = styleSet.join('; ');
+					});
+					paragraphInfo.ranges.forEach(function(para) {
+						if ((para.userdata[16] & 16) !== 0) {
+							// TODO: handle images
+						}
+						else {
+							var d = document.createElement('DIV');
+							var styles = charInfo.slice(para.offset, para.offset + para.length);
+							if (styles.ranges.length >= 1 && (styles.ranges.length === 1
+									|| styles.ranges[0].offset !== para.offset
+									|| styles.ranges[0].length !== para.length)) {
+								var pos = para.offset;
+								for (var i = 0; i < styles.ranges.length; i++) {
+									var range = styles.ranges[i];
+									if (range.offset > pos) {
+										var preData = textData.subarray(pos, range.offset);
+										var preText = String.fromCharCode.apply(null, preData);
+										d.appendChild(document.createTextNode(preText));
+									}
+									var span = document.createElement('SPAN');
+									span.style.cssText = range.userdata.style;
+									var inData = textData.subarray(range.offset, range.offset + range.length);
+									var inText = String.fromCharCode.apply(null, inData);
+									span.appendChild(document.createTextNode(inText));
+									d.appendChild(span);
+									pos = range.offset + range.length;
+								}
+								if (pos < (para.offset + para.length)) {
+									var postData = textData.subarray(pos, para.offset + para.length);
+									var postText = String.fromCharCode.apply(null, postData);
+									d.appendChild(document.createTextNode(postText));
 								}
 							}
-						});
+							else {
+								if (styles.ranges.length === 1) {
+									d.style.cssText = styles.ranges[0].userdata.style;
+								}
+								var paraData = textData.subarray(para.offset, para.offset + para.length);
+								var paraText = String.fromCharCode.apply(null, paraData);
+								d.appendChild(document.createTextNode(paraText));
+							}
+							switch(para.userdata[1] & 3) {
+								case 1: d.style.textAlign = 'center'; break;
+								case 2: d.style.textAlign = 'right'; break;
+								case 3: d.style.textAlign = 'justify'; break;
+							}
+							d.innerHTML = d.innerHTML.replace(/\n/g, '<br>');
+							self.article.appendChild(d);
+						}
+					});
 				})
 				.then(function() {
 					return self.article;
